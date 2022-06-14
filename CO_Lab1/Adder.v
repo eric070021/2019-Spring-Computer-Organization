eb7a3a4bd56e0f9c8bd39b86a72af23c88976db2
Eric Hsiao <80262378+eric070021@users.noreply.github.com>
2022-06-15 00:59:58 +0800

    Delete CO_Lab1 directory

--- CO_Lab1/Adder.v
+++
@@ -1,9 +0,0 @@
-module Adder(
-             input  [32-1:0] src1_i,
-	         input  [32-1:0] src2_i,
-	         input [32-1:0] sum_o
-	         );
-
-assign sum_o = src1_i + src2_i;
-
-endmodule
--- CO_Lab1/Shift_Left_Two_32.v
+++
@@ -1,8 +0,0 @@
-module Shift_Left_Two_32(
-                         input [32-1:0] data_i,
-                         output [32-1:0] data_o
-                         );
-
-assign data_o = {data_i[29:0],2'b00};
-
-endmodule
